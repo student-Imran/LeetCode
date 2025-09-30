0defe888fd8cb0cfe644851e4d7acce026692ab8
student-Imran <ahmedimranmd55@gmail.com>
2025-09-30 15:18:30 +0600

    Solved

--- Single-Element-In-Sorted-Array.cpp
+++ Single-Element-In-Sorted-Array.cpp
@@ -4,7 +4,40 @@ using namespace std;
 class Solution {
 public:
     int singleNonDuplicate(vector<int>&nums) {
-        
+        if(nums.size()==1 || nums[0]!=nums[1]){
+            return nums[0];
+        }
+        else if(nums[nums.size()-1]!=nums[nums.size()-2]){
+            return nums[nums.size()-1];
+        }
+      int l =0;
+      int r = nums.size()-1;
+      // cout<<count<<'\n';
+      while(l<=r){
+        int mid = (l+r)/2;
+        // cout<<l<<" "<<mid<<" "<<r<<'\n';
+        if(nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]){
+            return nums[mid];
+        }
+        else if(mid%2==0){
+            if(nums[mid]==nums[mid-1]){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        else{
+            if(nums[mid]==nums[mid-1]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+       
+      }   
+      return 0;
     }
 };
 void solve()
